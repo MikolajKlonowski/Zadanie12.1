f85750f75352380c76e02f9fb25b57b2e6120d05
Mikołaj Klonowski <nick-93@o2.pl>
2018-07-01 14:08:49 +0200

    Zadanie41 zmoana aby było 50 liczb unikalnych

--- src/Zbior.java
+++ src/Zbior.java
@@ -1,29 +1,41 @@
-import java.util.Iterator;
-import java.util.Random;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Zbior {
     public static void main(String[] args) {
-        TreeSet<Integer> set = new TreeSet<> ( );
-        Random random = new Random ();
+        TreeSet<Integer> set = new TreeSet<> ();
+        Random random = new Random ( );
         int suma = 0;
+
+
         for (int i = 0; i < 50; i++) {
+
             int a = random.nextInt (1000);
+            while (set.contains (a)) {
+                a = random.nextInt (1000);
+            }
             set.add (a);
-            suma+=a;
+            suma += a;
         }
-        System.out.println ("najmniejsza liczba "+set.first () );
-        System.out.println ("najwieksza liczba "+set.last () );
-        int srednia = suma/set.size ();
-        System.out.println ("Srednia "+srednia );
+
+
+
+
+
+               System.out.println ("najmniejsza liczba "+set.first () );
+      System.out.println ("najwieksza liczba "+set.last ()) ;
+        int srednia = suma / set.size ( );
+        System.out.println ("Srednia " + srednia);
 
         Iterator<Integer> iterator = set.iterator ( );
-        while (iterator.hasNext ()){
-            if (iterator.next () >srednia){
-                int next = iterator.next ();
-                System.out.println (next );
+        while (iterator.hasNext ( ))
+
+        {
+            if (iterator.next ( ) > srednia) {
+                int next = iterator.next ( );
+                System.out.println (next);
             }
         }
+
 
     }
 }
